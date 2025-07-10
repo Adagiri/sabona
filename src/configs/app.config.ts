43e63242ac79b24dd1ba47829c7648b9e9f5c08a
Ridwan <ibrahimridwan47@gmail.com>
2025-07-10 14:18:33 +0300

    added env.config.template

--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -24,8 +24,6 @@ const AppConfig = {
         BUCKET: process.env.APP_AWS_BUCKET,
         BUCKET_BASE_URL: process.env.APP_AWS_BUCKET_BASE_URL,
         STS_ROLE_ARN: process.env.APP_AWS_STS_ROLE_ARN,
-        QUEUE_URL: process.env.APP_AWS_QUEUE_URL,
-        SES_FROM_EMAIL: process.env.APP_AWS_SES_FROM_EMAIL,
     },
     TWILIO: {
         ACCOUNT_SID: process.env.APP_TWILIO_ACCOUNT_SID,
@@ -35,11 +33,6 @@ const AppConfig = {
     OAUTH: {
         GOOGLE: process.env.APP_GOOGLE_OAUTH_ENDPOINT,
         APPLE: process.env.APP_APPLE_OAUTH_ENDPOINT,
-    },
-    AGORA: {
-        APP_ID: process.env.APP_AGORA_APP_ID,
-        APP_CERTIFICATE: process.env.APP_AGORA_APP_CERTIFICATE,
-        LIVE_SESSION_TOKEN_EXPIRATION: Number(process.env.APP_AGORA_LIVE_SESSION_TOKEN_EXPIRATION),
     },
 
     FIREBASE: {
